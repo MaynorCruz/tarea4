a10a49f08b8ba7442b8666323a2551fc8b4c62a3
MaynorCruz <Maynorcruz@unitec.edu>
2014-09-22 08:13:01 -0600

    Lo_realize

--- Circulo.java
+++ Circulo.java
@@ -2,25 +2,32 @@
 public class Circulo {
 	double radio;
 	Circulo(double radio)
+	// Declaramos el radio que nos pide
 	{
 		this.radio = radio;
 	}
 	double getRadio()
+	// Devolvemos el radio con return 
 	{
 		return radio;
 	}
 	
 	void setRadio(double radio)
+	// utilizamos el radio via la oparacion matematica de PI
 	{
 		this.radio = Math.PI;
 	}
 
 	double getPerimetro()
+	// Utilizaremos Return
+	// Multiplicamos Pi*radio
 	{
 		return 2*Math.PI*radio;
 	}
 	
 	double getArea()
+	// Volvemos a utilizar return
+	// HAcemos la multiplicacion
 	{
 		return Math.PI*Math.pow(radio,2);
 	}
